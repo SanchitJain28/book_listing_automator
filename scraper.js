33bed9fb8fc91ad88ba6e29e6d2cd16c4e97dc43
SanchitJain28 <mythichuman28@gmail.com>
2026-05-03 23:19:27 +0530

    removed fixed view ports

--- scraper.js
+++ scraper.js
@@ -57,7 +57,6 @@ function cleanAndCheckMRP(priceStr, mrpStr) {
    const context = await browser.newContext({
      userAgent:
        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-     viewport: { width: 1920, height: 1080 }, 
    });
 
 
